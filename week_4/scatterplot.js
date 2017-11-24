a9bb39e885da97a45dca7e1039398c4339220d02
lottege <siEwu6xy>
2017-11-24 11:44:16 +0100

    ok

--- week_4/scatterplot.js
+++ week_4/scatterplot.js
@@ -1,4 +1,20 @@
+/**
+* scatterplot.js
+* Lotte Geeraedts
+* Data Processing
+* Studentnummer: 10529748
+* Location: https://github.com/lottege/data_processing/blob/master/week_3/scatterplot.js
+*
+* This function is used in scatterplot.html to make a scatterplot
+* using data from data1.json. This visualizes the correlation between average life expectancy and average well being.
+*
+* To make this code, the website https://bl.ocks.org/mbostock/3887118 was frequently used.
+**/
+
+
+// execute script once page is fully loaded
 window.onload = function() {
+    // initialize the elements of the plot
     var margin = {top: 20, right: 150, bottom: 40, left: 40},
         width = 960 - margin.left - margin.right,
         height = 500 - margin.top - margin.bottom;
@@ -34,73 +50,78 @@ window.onload = function() {
             return d.country
         });
 
-svg.call(tip);
+    svg.call(tip);
 
-d3.json("data1.json", function(error, data) {
-  if (error) throw error;
+    // use data from file
+    d3.json("data1.json", function(error, data) {
+        if (error) throw error;
 
-  data.forEach(function(d) {
-    d.average_life_expectancy = parseFloat(d.average_life_expectancy.replace(/,/, '.'));
-    d.average_well_being = parseFloat(d.average_well_being.replace(/,/, '.'));
-  });
+        // turn , into . to interpret values as float
+        data.forEach(function(d) {
+        d.average_life_expectancy = parseFloat(d.average_life_expectancy.replace(/,/, '.'));
+        d.average_well_being = parseFloat(d.average_well_being.replace(/,/, '.'));
+        });
 
-  x.domain(d3.extent(data, function(d) { return d["average_life_expectancy"]; })).nice();
-  y.domain(d3.extent(data, function(d) { return d["average_well_being"]; })).nice();
+        // set domain of the axes
+        x.domain(d3.extent(data, function(d) { return d["average_life_expectancy"]; })).nice();
+        y.domain(d3.extent(data, function(d) { return d["average_well_being"]; })).nice();
 
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis)
-    .append("text")
-      .attr("class", "label")
-      .attr("x", width)
-      .attr("y", -6)
-      .style("text-anchor", "end")
-      .text("average life expectancy");
+        // add x axis to svg
+        svg.append("g")
+          .attr("class", "x axis")
+          .attr("transform", "translate(0," + height + ")")
+          .call(xAxis)
+        .append("text")
+          .attr("class", "label")
+          .attr("x", width)
+          .attr("y", -6)
+          .style("text-anchor", "end")
+          .text("average life expectancy");
 
-  svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-    .append("text")
-      .attr("class", "label")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("average well-being")
+        // add y axis to svg
+        svg.append("g")
+          .attr("class", "y axis")
+          .call(yAxis)
+        .append("text")
+          .attr("class", "label")
+          .attr("transform", "rotate(-90)")
+          .attr("y", 6)
+          .attr("dy", ".71em")
+          .style("text-anchor", "end")
+          .text("average well-being")
 
-  var dots = svg.selectAll(".dot")
-      .data(data)
-      .enter().append("circle")
-      .attr("class", "dot")
-      .attr("r", 3.5)
-      .attr("cx", function(d, i) { return x(d.average_life_expectancy); })
-      .attr("cy", function(d, i) { return y(d.average_well_being); })
-      .attr("y", function(d, i) { return y(d.average_well_being); })
-      .attr("x", function(d, i) { return x(d.average_life_expectancy); })
-      .style("fill", function(d) { return color(d.region); })
-        .on("mouseover", tip.show)
-        .on("mouseout", tip.hide);
+        // initialize dots and their interactivity
+        var dots = svg.selectAll(".dot")
+          .data(data)
+        .enter().append("circle")
+          .attr("class", "dot")
+          .attr("r", 3.5)
+          .attr("cx", function(d, i) { return x(d.average_life_expectancy); })
+          .attr("cy", function(d, i) { return y(d.average_well_being); })
+          .style("fill", function(d) { return color(d.region); })
+            .on("mouseover", tip.show)
+            .on("mouseout", tip.hide);
 
+        // initialize legend
+        var legend = svg.selectAll(".legend")
+          .data(color.domain())
+        .enter().append("g")
+          .attr("class", "legend")
+          .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
 
-  var legend = svg.selectAll(".legend")
-      .data(color.domain())
-    .enter().append("g")
-      .attr("class", "legend")
-      .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+        // add rectangles to legend
+        legend.append("rect")
+          .attr("x", width - 18)
+          .attr("width", 18)
+          .attr("height", 18)
+          .style("fill", color);
 
-  legend.append("rect")
-      .attr("x", width - 18)
-      .attr("width", 18)
-      .attr("height", 18)
-      .style("fill", color);
-
-  legend.append("text")
-      .attr("x", width + 10)
-      .attr("y", 9)
-      .attr("dy", ".35em")
-      .style("text-anchor", "start")
-      .text(function(d) { return d; });
-});
-
+        // add text to legend
+        legend.append("text")
+          .attr("x", width + 10)
+          .attr("y", 9)
+          .attr("dy", ".35em")
+          .style("text-anchor", "start")
+          .text(function(d) { return d; });
+    });
 }
--- week_4/svg.js
+++ week_4/svg.js
@@ -1,8 +1,11 @@
 d3.xml("test.svg", "image/svg+xml", function(error, xml) {
     if (error) throw error;
     document.body.appendChild(xml.documentElement);
-    document.getElementById("kleur1").style.fill = "blue";
-    document.getElementById("kleur2").style.fill = "red";
-    document.getElementById("kleur3").style.fill = "green";
+    document.getElementById("kleur1").style.fill = "#ccece6";
+    document.getElementById("kleur2").style.fill = "#99d8c9";
+    document.getElementById("kleur3").style.fill = "#66c2a4";
+    document.getElementById("kleur4").style.fill = "#41ae76";
+    document.getElementById("kleur5").style.fill = "#238b45";
+    document.getElementById("kleur6").style.fill = "#005824";
 });
 
